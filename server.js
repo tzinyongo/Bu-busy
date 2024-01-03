9f9f4c501a81f2b13c935c322471bb376e4de59a
anmar <anmar@bu.edu>
2024-01-02 21:19:50 -0500

    Initial server setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000; 
+
+app.get('/', (req, res) => {
+    res.send('Gym Occupancy Tracker Home');
+  });
+  
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
