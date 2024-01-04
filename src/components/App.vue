bbb2261cba81398b3d502301dcc2b10ac848dd6d
tzinyongo <51459077+tzinyongo@users.noreply.github.com>
2024-01-04 16:25:14 -0500

    hello

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,40 +1,37 @@
 <template>
-  <div id="app">
+  <div id="app" class="flex-container">
     <div class="header">
       <h1>BU-Busy</h1>
     </div>
 
+    <div class="box-container">
+      <!-- Router Links -->
+      <router-link v-if="isRootPage" to="/another-page" class="gym-info-box">
+        <div>
+          <h1>Weight Room</h1>
+          <p>Welcome to our school's gym! Check out the facilities and enjoy your workout.</p>
+          Click me to go to another page
+        </div>
+      </router-link>
+
+      <router-link v-if="isRootPage" to="/fourth-page" class="courts">
+        <div>
+          <h1>Courts</h1>
+          <p>Check the business on the courts of our BU Fitrec</p>
+          Click me to go to another page
+        </div>
+      </router-link>
+
+      <router-link v-if="isRootPage" to="/third-page" class="evaluate">
+        <div>
+          <h1>Rate our Facility</h1>
+        </div>
+      </router-link>
+    </div>
+
+    <!-- Router Views -->
     <router-view v-if="isRootPage"></router-view>
-
-    <router-link v-if="isRootPage" to="/another-page" class="gym-info-box">
-      <div>
-        <h1>Weight Room</h1>
-        <p>
-          Welcome to our school's gym! Check out the facilities and enjoy your workout.
-        </p>
-        Click me to go to another page
-      </div>
-    </router-link>
-
-    <router-view v-if="isRootPage"></router-view>
-    <router-link v-if="isRootPage" to="/fourth-page" class="courts">
-      <div> 
-        <h1> Courts </h1>
-      </div>
-    </router-link>
-
-    <router-view v-if="isRootPage"></router-view>
-    <router-link v-if="isRootPage" to="/third-page" class="evaluate">
-      <div> 
-        <h1> Rate our Facility </h1>
-      </div>
-    
-    </router-link>
-
-    
-
     <router-view></router-view>
-    
   </div>
 </template>
 
@@ -46,16 +43,27 @@ export default {
       return this.$route.path === '/';
     }
   }
-  
 };
-</script >
+</script>
 
 <style scoped>
+.flex-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.box-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 20px; /* Optional: Add some margin between header and boxes */
+}
+
 .header {
-  background-color:none; /* Green background color for the header */
+  background-color: none;
   padding: 20px;
   text-align: center;
-
 }
 
 .gym-info-box {
@@ -117,3 +125,6 @@ export default {
 
 
 </style>
+
+
+
