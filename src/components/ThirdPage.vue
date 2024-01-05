276d8b7f9848c855cd0d4145c2fc47ce80f81b98
tzinyongo <51459077+tzinyongo@users.noreply.github.com>
2024-01-05 11:46:10 -0500

    another push

--- src/components/ThirdPage.vue
+++ src/components/ThirdPage.vue
@@ -39,26 +39,19 @@ export default {
 <style scoped>
 .back {
   position: absolute;
-  top: 20px;
-  left: 20px;
   display: inline-block;
-  padding: 10px 20px;
-  font-size: 16px;
+  display: inline-block;
+  top: 10px;
+  left: 10x;
+  display: block;
+  background-color: grey;
   text-align: center;
   text-decoration: none;
-  color: #fff;
-  background-color: rgba(12, 12, 12, 0.4); /* Subtle transparent gray */
-  border: 2px solid rgba(128, 128, 128, 0.5); /* Slightly transparent border */
+  color: inherit;
   border-radius: 5px;
-  transition: background-color 0.3s ease, transform 0.2s ease, border 0.3s ease;
+  padding: 5px 10px;
 }
 
-.back:hover {
-  background-color: rgba(169, 169, 169, 0.6); /* Darker gray on hover */
-  border: 2px solid rgba(128, 128, 128, 0.7); /* Darker border on hover */
-  transform: scale(1.05); /* Slight scale on hover for depth effect */
-  cursor: pointer;
-}
 .content {
   margin-top: 40px; /* Adjusted margin-top */
   margin-left: 10px;
