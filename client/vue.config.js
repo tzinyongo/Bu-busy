3ff990395a88ace2556b63b6ca7af3d4a44f2f20
tzinyongo <51459077+tzinyongo@users.noreply.github.com>
2024-01-30 17:09:21 -0500

    Updates

--- client/vue.config.js
+++
@@ -1,6 +0,0 @@
-const { defineConfig } = require('@vue/cli-service');
-
-module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/Bu-busy/' : '/',
-  //transpileDependencies: ['vue-awesome'], // Add any dependencies that need to be transpiled
-});
