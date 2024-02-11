a7c9ac4e18ef792db965de0e42ab330faab5d845
tzinyongo <51459077+tzinyongo@users.noreply.github.com>
2024-02-11 13:31:54 -0500

    changes the package.json

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,4 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/BU-BUSY/client/dist/index.html' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/Bu-busy/' : '/',
 };
