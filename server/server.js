03bac0b4df1a8c95f4e14bdb866a79dc424ea846
tzinyongo <51459077+tzinyongo@users.noreply.github.com>
2024-03-08 01:53:38 -0500

    Added some changes to the server

--- server/server.js
+++ server/server.js
@@ -65,51 +65,40 @@ const calculateAveragerating = async (facilityId, res) =>
 };
 
 let lastResetTime = new Date();
-const updateAndResetAverageRating = async (facilityId, res) => {
+
+/*const updateAndResetAverageRating = async (facilityId, res) => {
   try {
     const currentDate = new Date();
     const startOfHour = new Date(currentDate);
     startOfHour.setMinutes(0, 0, 0);
 
-    // Check if it's a new hour, and if so, reset the basis
-    if (currentDate.getHours() !== lastResetTime.getHours()) {
-      lastResetTime = new Date();
-      
-      // Find ratings within the last hour
-      const ratings = await Rating.find({
-        facility_id: facilityId,
-        createdAt: { $gte: startOfHour },
-      });
+    // Find ratings within the last hour
+    const ratings = await Rating.find({
+      facility_id: facilityId,
+      createdAt: { $gte: startOfHour },
+    });
 
-      // Calculate the average rating
-      let average = 1;
-      res.json({ average: average.toFixed(2) });
+    // Calculate the average rating
+    const average = (ratings.reduce((acc, { rating }) => acc + rating, 0) / (ratings.length || 1)).toFixed(2);
+
+    // Send the response only if it hasn't been sent already
+    if (!res.headersSent) {
+      res.json({ average: average });
     } else {
-      // Continue with the existing logic when it's not a new hour
-      const ratings = await Rating.find({
-        facility_id: facilityId,
-        createdAt: { $gte: startOfHour },
-      });
-
-      let average = 1;
-      if (ratings.length > 0) {
-        average =
-          ratings.reduce((acc, { rating }) => acc + rating, 0) /
-          (ratings.length || 1);
-      }
-
-      res.json({ average: average.toFixed(2) });
+      console.log('Response headers already sent');  // Log a message (optional)
     }
   } catch (error) {
     res.status(500).json({ message: "Error fetching the average rating", error: error });
   }
-};
+};*/
+
+
 
 // GET Route for retrieving upstairs weigh room average rating
 app.get('/api/ratings/average/weight-room1', async (req, res) => {
   try {
-    const average = await updateAndResetAverageRating('6599fa67d85aa7b7734fef3d', res); // weight room 1 (upstairs)
-    //const average = ratings.reduce((acc, { rating }) => acc + rating, 0) / (ratings.length || 1);
+    const ratings = await Rating.find({ facility_id: '6599fa67d85aa7b7734fef3d' }); // weight room 1 (upstairs)
+    const average = ratings.reduce((acc, { rating }) => acc + rating, 0) / (ratings.length || 1);
     res.json({ average: average.toFixed(2) }); // Send the average as a response
   } catch (error) {
     res.status(500).json({ message: "Error fetching the average rating", error: error });
@@ -119,9 +108,10 @@ app.get('/api/ratings/average/weight-room1', async (req, res) => {
 // GET Route for retrieving lower weigh room average rating
 app.get('/api/ratings/average/weight-room2', async (req, res) => {
   try {
-    const average = await updateAndResetAverageRating('6599fa7dd85aa7b7734fef3f', res); // weight room 2 (downstairs)
-    //const average = ratings.reduce((acc, { rating }) => acc + rating, 0) / (ratings.length || 1);
-    res.json({ average: average.toFixed(2) }); // Send the average as a response
+    const ratings = await Rating.find({ facility_id: '6599fa7dd85aa7b7734fef3f' }); // weight room 1 (upstairs)
+    const average = ratings.reduce((acc, { rating }) => acc + rating, 0) / (ratings.length || 1);
+    res.json({ average: average.toFixed(2) });
+    // Send the average as a response
   } catch (error) {
     res.status(500).json({ message: "Error fetching the average rating", error: error });
   }
@@ -130,9 +120,11 @@ app.get('/api/ratings/average/weight-room2', async (req, res) => {
 // GET Route for retrieving lower weigh room average rating
 app.get('/api/ratings/average/upper-courts', async (req, res) => {
   try {
-    const average = await updateAndResetAverageRating('659a0affc0d15d547c126925',res); // weight room 2 (downstairs)
-    //const average = ratings.reduce((acc, { rating }) => acc + rating, 0) / (ratings.length || 1);
-    res.json({ average: average.toFixed(2) }); // Send the average as a response
+    const ratings = await Rating.find({ facility_id: '659a0affc0d15d547c126925' }); // weight room 1 (upstairs)
+    const average = ratings.reduce((acc, { rating }) => acc + rating, 0) / (ratings.length || 1);
+    res.json({ average: average.toFixed(2) });
+
+     // Send the average as a response
   } catch (error) {
     res.status(500).json({ message: "Error fetching the average rating", error: error });
   }
@@ -141,26 +133,29 @@ app.get('/api/ratings/average/upper-courts', async (req, res) => {
 // GET Route for retrieving lower weigh room average rating
 app.get('/api/ratings/average/lower-courts', async (req, res) => {
   try {
-    const average = await updateAndResetAverageRating('659b382f10a04f1ccc8f2e6b',res); // weight room 2 (downstairs)
-    //const average = ratings.reduce((acc, { rating }) => acc + rating, 0) / (ratings.length || 1);
-    res.json({ average: average.toFixed(2) }); // Send the average as a response
+
+    const ratings = await Rating.find({ facility_id: '659b382f10a04f1ccc8f2e6b' }); // weight room 1 (upstairs)
+    const average = ratings.reduce((acc, { rating }) => acc + rating, 0) / (ratings.length || 1);
+    res.json({ average: average.toFixed(2) });
+
+     // Send the average as a response
   } catch (error) {
     res.status(500).json({ message: "Error fetching the average rating", error: error });
   }
 });
 
 
-/*cron.schedule('* * * * *', async () => {
+cron.schedule('*/1 * * * *', async () => {
 
   console.log('Cron job is running!');
-  await updateAndResetAverageRating('6599fa67d85aa7b7734fef3d'); // Facility ID for weight room 1 (upstairs)
-  await updateAndResetAverageRating('6599fa7dd85aa7b7734fef3f'); // Facility ID for weight room 2 (downstairs)
-  await updateAndResetAverageRating('659a0affc0d15d547c126925'); // Facility ID for upper courts
-  await updateAndResetAverageRating('659b382f10a04f1ccc8f2e6b'); // Facility ID for lower courts
+  await updateAndResetAverageRating('6599fa67d85aa7b7734fef3d', res); // Facility ID for weight room 1 (upstairs)
+  await updateAndResetAverageRating('6599fa7dd85aa7b7734fef3f', res); // Facility ID for weight room 2 (downstairs)
+  await updateAndResetAverageRating('659a0affc0d15d547c126925', res); // Facility ID for upper courts
+  await updateAndResetAverageRating('659b382f10a04f1ccc8f2e6b', res); // Facility ID for lower courts
 }, {
   scheduled: true,
   timezone: "America/New_York", // Adjust the timezone as needed
-});*/
+});
 
 // Start the server after setting up MongoDB and routes
 const PORT = process.env.PORT || 3000;
